a29f57de09d9d58412b0a1d406e5ee412fdc0a17
boris6 <sijanb@outlook.com>
2023-01-01 18:10:04 +0100

    Add HEAD and OPTIONS requests

--- CompanyEmployees.Presentation/Controllers/EmployeesController.cs
+++ CompanyEmployees.Presentation/Controllers/EmployeesController.cs
@@ -20,6 +20,7 @@ public class EmployeesController : ControllerBase
     }
 
     [HttpGet]
+    [HttpHead]
     [ServiceFilter(typeof(ValidateMediaTypeAttribute))]
     public async Task<IActionResult> GetEmployeesForCompany(Guid companyId,
         [FromQuery] EmployeeParameters employeeParameters)
